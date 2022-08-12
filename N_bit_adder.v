d25ead7c70baec38ba162270d3f5bdd0a9756b52
lion42 <74356913+lion42-cat@users.noreply.github.com>
2022-08-12 13:55:11 +0900

    Update N_bit_adder.v

--- N_bit_adder.v
+++ N_bit_adder.v
@@ -1,34 +1,38 @@
-module N_bit_adder(input1,input2,answer);
-parameter N=32;         //32 bit adder
-input [N-1:0] input1,input2;
-   output [N-1:0] answer;
-   wire  carry_out;
-   wire [N-1:0] carry;
-   genvar i;
-	generate 
-	for(i=0;i<N;i=i+1)
-     	begin: generate_N_bit_Adder
-	if(i==0) 
-	half_adder f(input1[0],input2[0],answer[0],carry[0]);
-	else
-	full_adder f(input1[i],input2[i],carry[i-1],answer[i],carry[i]);
-	end			
-    assign carry_out = carry[N-1];
-   endgenerate
-endmodule 
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+module N_bit_adder(clk, rst, input1_in, input2_in, answer_out);
+parameter N=32; //32bit adder
+input clk, rst;
+input [N-1:0] input1_in, input2_in;
+output [N-1:0] answer_out;
 
+wire [N-1:0] input1, input2;
+wire [N-1:0] answer;
 
-module half_adder(x,y,s,c);
-   input x,y;
-   output s,c;
-   assign s=x^y;
-   assign c=x&y;
-endmodule // half adder
+wire carry_out;
+wire [N-1:0] carry;
 
+//in
+	dff input1_a(input1_in, clk, rst, input1);
+	dff input2_a(input2_in, clk, rst, input2);
+	
+//calculate
+	genvar i;
 
-module full_adder(x,y,c_in,s,c_out);
-   input x,y,c_in;
-   output s,c_out;
- assign s = (x^y) ^ c_in;
- assign c_out = (y&c_in)| (x&y) | (x&c_in);
-endmodule // full_adder
+	generate
+	for(i=0; i<N; i=i+1)
+	begin : generate_N_bit_Adder
+		if(i==0)
+		begin
+		half_adder f(clk, rst, input1[0], input2[0], answer[0], carry[0]);
+		end
+		else
+		full_adder f_i(clk, rst, input1[i], input2[i], carry[i-1], answer[i], carry[i]);
+	end
+	assign carry_out = carry[N-1];
+	endgenerate
+	
+//out
+	assign answer_out = answer;
+	
+endmodule
