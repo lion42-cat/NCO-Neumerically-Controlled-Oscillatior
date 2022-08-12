e4a2b4beb14e9d8f65ea429f99e0aa00a7867dc0
lion42 <74356913+lion42-cat@users.noreply.github.com>
2022-08-12 13:58:09 +0900

    Update tb_N_bit_adder.v

--- tb_N_bit_adder.v
+++ tb_N_bit_adder.v
@@ -1,28 +1,42 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
 module tb_N_bit_adder;
- // Inputs
- reg [31:0] input1;
- reg [31:0] input2;
- // Outputs
- wire [31:0] answer;
+reg clk, rst;
+reg [31:0] input1_in, input2_in;
+wire [31:0] answer_out;
 
- N_bit_adder uut (
-  .input1(input1), 
-  .input2(input2), 
-  .answer(answer)
- );
+N_bit_adder u3(
+.clk(clk),
+.rst(rst),
+.input1_in(input1_in),
+.input2_in(input2_in),
+.answer_out(answer_out)
+);
 
- initial begin
-  input1 = 4232;
-  input2 = 4232;
-  #100;
-  input1 = 2424;
-  input2 = 3355;
-  #100;
-  input1 = 1000;
-  input2 = 1111;
-  #100;
- end
-      
+always begin #1 clk=~clk; end
+
+initial
+	begin
+		clk=1; rst=1; input1_in=0; input2_in=0;
+		#2 rst=0; input1_in=1; input2_in=0;
+		#2 rst=0; input1_in=2; input2_in=0;
+		#2 rst=0; input1_in=4; input2_in=4;
+		#2 rst=0; input1_in=8; input2_in=8;
+		#2 rst=0; input1_in=16; input2_in=16;
+		#2 rst=0; input1_in=100; input2_in=100;
+		#2 rst=0; input1_in=1000; input2_in=1000;
+		#2 rst=0; input1_in=20000; input2_in=20000;
+		#2 rst=0; input1_in=300000; input2_in=300000;
+		#2 rst=0; input1_in=4000000; input2_in=4000000;
+		#2 rst=0; input1_in=50000000; input2_in=50000000;
+		#2 rst=0; input1_in=600000000; input2_in=600000000;
+		#2 rst=0; input1_in=1; input2_in=1;
+		#2 rst=0; input1_in=2; input2_in=2;
+		#2 rst=0; input1_in=3; input2_in=3;
+		#2 rst=0; input1_in=4; input2_in=4;
+		#2 rst=0; input1_in=5; input2_in=5;
+		
+		#20 $finish;
+	end
+
 endmodule
-
-
