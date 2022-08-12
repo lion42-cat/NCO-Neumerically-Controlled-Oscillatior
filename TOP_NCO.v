69cea011cb996ee1723e88dd48795d7c261880a3
lion42 <74356913+lion42-cat@users.noreply.github.com>
2022-08-12 13:56:26 +0900

    Update TOP_NCO.v

--- TOP_NCO.v
+++ TOP_NCO.v
@@ -1,18 +1,16 @@
-`timescale 1ns / 1ps
+`timescale 1ns / 1fs
 //////////////////////////////////////////////////////////////////////////////////
-module TOP_NCO(clk_top, rst_top ,NCO_in ,NCO_out);
+module TOP_NCO(clk_top, rst_top ,NCO_in, NCO_enable ,NCO_sin_out, NCO_cos_out);
 input clk_top, rst_top;
 input [31:0] NCO_in;
-output [7:0] NCO_out;
+input NCO_enable;
+output [15:0] NCO_sin_out, NCO_cos_out;
 
-wire [31:0] Phase_accumulator_out;
-wire [3:0] Quantizer32to4_out;
+wire [31:0] Phase_accumulator_out; //go to Quantizer
+wire [31:0] Quantizer32to16_out; //go to sin_cos_rom
 
-Phase_accumulator Phase_accumulator1(clk_top, rst_top, NCO_in, Phase_accumulator_out);
-
-Quantizer32to4 Quantizer32to4_1(Phase_accumulator_out, Quantizer32to4_out);
-
-sin_rom sin_rom_1(Quantizer32to4_out, NCO_out);
-
+Phase_accumulator Phase_accumulator_1 (clk_top, rst_top, NCO_in, NCO_enable, Phase_accumulator_out);
+Quantizer32to16 Quantizer32to16_1 (clk_top, rst_top, Phase_accumulator_out, Quantizer32to16_out);
+sin_cos_rom sin_cos_rom_1 (clk_top, rst_top, Quantizer32to16_out, NCO_sin_out, NCO_cos_out);
 
 endmodule
