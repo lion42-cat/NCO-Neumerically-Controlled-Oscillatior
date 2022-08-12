70d4393d75134a3ca9b3c710b77ba9fe4ab347b5
lion42 <74356913+lion42-cat@users.noreply.github.com>
2022-08-12 13:57:51 +0900

    Update tb_Phase_accumulator.v

--- tb_Phase_accumulator.v
+++ tb_Phase_accumulator.v
@@ -1,25 +1,26 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-//모듈 선언
 module tb_Phase_accumulator;
-//신호 및 변수 선언
-reg clk;
-reg reset;
-reg [31:0] Phase_accumulator_in;
-wire [31:0] Phase_accumulator_out;
+	reg clk, rst;
+	reg [31:0] Phase_accumulator_in;
+	reg Phase_accumulator_write_enable_in;
+	wire [31:0] Phase_accumulator_out;
+	
+	Phase_accumulator u6(.clk(clk), .rst(rst),
+	.Phase_accumulator_in(Phase_accumulator_in),
+	.Phase_accumulator_write_enable_in(Phase_accumulator_write_enable_in),
+	.Phase_accumulator_out(Phase_accumulator_out)
+	);
+	
+	always #(1) clk = ~clk;
+	always #(2) begin rst = 0; Phase_accumulator_write_enable_in = 1; Phase_accumulator_in = 1; end//계속 더해짐
+	
+	initial begin
+		 clk = 1; rst = 1; Phase_accumulator_write_enable_in=0; Phase_accumulator_in=0; //초기 신호 초기화
+		#1 rst = 0;
 
-//검증대상 호출
-Phase_accumulator P_A1 (.clk(clk), .reset(reset),
-.Phase_accumulator_in(Phase_accumulator_in), 
-.Phase_accumulator_out(Phase_accumulator_out)
-);
-
-//신호입력을 통해 검증대상의 결과 확인
-always #(1) clk = ~clk;
-
-initial begin
-	clk = 0; reset = 1; Phase_accumulator_in=0; //신호 초기화
-	#5 reset = 0; Phase_accumulator_in = 1;
-    #100 $finish;
-end
+		// Phase_accumulator_write_enable_in = 0; Phase_accumulator_in = 1; //write enable 끔
+		#30	 $finish;
+	end
+	
 endmodule
